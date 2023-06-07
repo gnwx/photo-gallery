04bdee44ef03911dbc2e06493b71b2adc56c7120
gnw <seimap_00@hotmail.com>
2023-06-07 20:08:44 +0300

    resize image

--- client/src/components/ImageCard.jsx
+++ client/src/components/ImageCard.jsx
@@ -7,8 +7,8 @@ const ImageCard = ({ image }) => {
       sx={{
         borderRadius: "8px",
         mt: 10,
-        maxWidth: "600px",
-        maxHeight: "600px",
+        width: "400px",
+        height: "400px",
         objectFit: "cover",
         boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
       }}
